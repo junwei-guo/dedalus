88eb51a8376ec6705a0c40bae4b6fcf49d240c99
Junwei Guo <guo_junwei@hotmail.com>
2025-07-08 19:02:07 -0600

    mac script working good now

--- tgv/mac.sh
+++ tgv/mac.sh
@@ -4,9 +4,10 @@
 git clone --branch tgv --single-branch https://github.com/junwei-guo/dedalus.git
 cd dedalus
 
+MPI_HOME=/Users/guo/homebrew/Cellar/open-mpi/5.0.7
+GCC_HOME=/Users/guo/homebrew/Cellar/gcc/15.1.0
+FFTW_HOME=/Users/guo/homebrew/Cellar/fftw/3.3.10_2
 
-
-module load  python/3.9.5 fftw/3.3.8   openmpi/4.0.6 
 echo $MPI_HOME
 echo $FFTW_HOME
 echo $GCC_HOME
@@ -30,9 +31,9 @@ rm -rf  dedalus-env
 python3 -m venv dedalus-env
 source dedalus-env/bin/activate
 python3 -m pip install numpy scipy sympy Cython mpi4py h5py     
-python3 -m pip install  docopt tblib coverage #pytest
+python3 -m pip install  docopt tblib coverage pytest setuptools
 
-python3 -m pip install pytest==7.4.4 
+#python3 -m pip install pytest==7.4.4 
 #pip install pytest==7.4.4 
 # petsc petsc4py docopt
 #pytest
@@ -44,3 +45,6 @@ python3 -c "import dedalus; print(dedalus.__version__)"
 
 
 mpirun -np 4 python3 -m dedalus test
+
+
+ python3 -m pip install matplotlib numexpr xarray py pytest-benchmark pytest-cov pytest-parallel
